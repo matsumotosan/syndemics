89928af0f25089d5ce2d0447a7a825813a944d05
Shion <smatsumoto97@gmail.com>
2025-01-17 11:29:58 -0500

    assume pdfs manually downloaded

--- extract_text.py
+++ extract_text.py
@@ -1,4 +1,3 @@
-import os
 from pathlib import Path
 
 import pymupdf
@@ -11,17 +10,15 @@ pdf_dir = "./SyndemicsResearchArticles"
 
 
 def main():
-    os.makedirs(output_dir, exist_ok=True)
-    os.makedirs(pdf_dir, exist_ok=True)
-
     # Download files from GDrive folder (limited to 50)
     # Either edit source code in virtual environment or download manually
-    # Easier to just download manually
-    # gdown.download_folder(URL, output=pdf_dir, quiet=False)
+    # os.makedirs(pdf_dir, exist_ok=True)
+    # gdown.download_folder(URL, output=pdf_dir, quiet=False) # easier to just download manually
 
     # Extract text for all PDFs
     path_pdf = Path(pdf_dir)
     path_output = Path(output_dir)
+    path_output.mkdir(parents=True, exist_ok=True)
 
     files = [f for f in path_pdf.iterdir() if f.is_file()]
 
@@ -29,9 +26,8 @@ def main():
         with pymupdf.open(f) as doc:
             text = chr(12).join([page.get_text() for page in doc])
 
-        basename = f.stem
-
         # write as a binary file to support non-ASCII characters
+        basename = f.stem
         Path(path_output / f"{basename}.txt").write_bytes(text.encode())
 
 
